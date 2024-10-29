d091b1bdb494ed62d40f43ad27844f0c6a65122e
decraenerobin <robin@esign.eu>
2024-10-29 13:56:07 +0100

    Update design header

--- src/Components/Header/header.jsx
+++ src/Components/Header/header.jsx
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import style from './header.module.css';
 import ROUTES from '../../Routes/routes';
 import { Link, useLocation } from 'react-router-dom';
+import {firebase} from '../../firebase';
+
 
 const Header = () => {
   const location = useLocation();
+  const [logoUrl, setLogoUrl] = useState(null);
+
+  useEffect(() => {
+    const fetchLogo = async () => {
+      try {
+        const url = await firebase.storage().ref('main/groot.webp').getDownloadURL();
+        setLogoUrl(url); 
+      } catch (error) {
+        console.error('Error fetching profile photo:', error);
+      }
+    };
+
+    fetchLogo();
+  }, []);
 
   return (
     <header>
       <nav>
-        <a href="/" className={style.name}>Logo</a>
+        <a href="/" className={style.name}><img src={logoUrl} alt='logo' className={style.logo}/></a>
         <div className={style.navRight}>
           <Link to={ROUTES.home} className={location.pathname === ROUTES.home ? style.activeNav : ''}>Home</Link>
           <Link to={ROUTES.aboutme} className={location.pathname === ROUTES.aboutme ? style.activeNav : ''}>Over mij</Link>
