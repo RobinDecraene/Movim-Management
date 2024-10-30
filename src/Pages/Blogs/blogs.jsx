9bfebdd7d3c483a5f25ea8f227a8ec72ec8bf9b9
decraenerobin <robin@esign.eu>
2024-10-30 14:53:43 +0100

    Add: blog page text & swiper

--- src/Pages/Blogs/blogs.jsx
+++ src/Pages/Blogs/blogs.jsx
@@ -1,11 +1,138 @@
 import React from 'react';
 import style from './blogs.module.css';
 import Page from '../../Components/Page/page';
+import Section from '../../Components/Section/section';
+import SmallestTitle from '../../Components/SmallestTitle/smallesttitle';
+import Title from '../../Components/Title/title';
+import Button from '../../Components/Button/button';
+import placeholder from '../../Components/Card/placeholder.jpg';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Mousewheel, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import Card from '../../Components/Card/card';
+
 
 const Blogs = () => {
     return (
       <Page>
-        
+        <Section>
+          <Swiper
+            style={{
+              '--swiper-pagination-color': '#0D1B2A',
+            }}
+            slidesPerView={1}
+            spaceBetween={100}
+            pagination={{ clickable: true }}
+            mousewheel={true}
+            modules={[Mousewheel, Pagination]}
+            className={style.mySwiper}
+          >
+
+            <SwiperSlide className={style.swiperSlide}>
+              <div className={style.swiperSlideText}>
+                <SmallestTitle>Featured</SmallestTitle>
+                <Title>Lorem ipsum dolor sit amet, consetetur</Title>
+                <p>
+                  sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, 
+                  sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet 
+                  clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem 
+                  ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt 
+                  ut labore et dolore magna aliquyam erat, sed diam voluptua.
+                </p>
+                <Button>Lees verder</Button>
+              </div>
+              <img src={placeholder} alt='blogpost' className={style.swiperSlideImg}/>
+            </SwiperSlide>
+
+            <SwiperSlide className={style.swiperSlide}>
+              <div className={style.swiperSlideText}>
+                <SmallestTitle>Featured</SmallestTitle>
+                <Title>Lorem ipsum dolor sit amet, consetetur</Title>
+                <p>
+                  sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, 
+                  sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet 
+                  clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem 
+                  ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt 
+                  ut labore et dolore magna aliquyam erat, sed diam voluptua.
+                </p>
+                <Button>Lees verder</Button>
+              </div>
+              <img src={placeholder} alt='blogpost' className={style.swiperSlideImg}/>
+            </SwiperSlide>
+
+            <SwiperSlide className={style.swiperSlide}>
+              <div className={style.swiperSlideText}>
+                <SmallestTitle>Featured</SmallestTitle>
+                <Title>Lorem ipsum dolor sit amet, consetetur</Title>
+                <p>
+                  sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, 
+                  sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet 
+                  clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem 
+                  ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt 
+                  ut labore et dolore magna aliquyam erat, sed diam voluptua.
+                </p>
+                <Button>Lees verder</Button>
+              </div>
+              <img src={placeholder} alt='blogpost' className={style.swiperSlideImg}/>
+            </SwiperSlide>
+          </Swiper>
+        </Section>
+
+        <Section>
+          <Title>Recente Blog Posts</Title>
+          <div className={style.blogCards}>
+            <Card
+            title={'Lorem Ipsum Dolor Sit Amet, Consetetur'}
+            children={'sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.'}
+            date={'30/10/2024'}
+            />
+
+            <Card
+            title={'Lorem Ipsum Dolor Sit Amet, Consetetur'}
+            children={'sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.'}
+            date={'30/10/2024'}
+            />
+
+            <Card
+            title={'Lorem Ipsum Dolor Sit Amet, Consetetur'}
+            children={'sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.'}
+            date={'30/10/2024'}
+            />
+
+            <Card
+            title={'Lorem Ipsum Dolor Sit Amet, Consetetur'}
+            children={'sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.'}
+            date={'30/10/2024'}
+            />
+
+            <Card
+            title={'Lorem Ipsum Dolor Sit Amet, Consetetur'}
+            children={'sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.'}
+            date={'30/10/2024'}
+            />
+
+            <Card
+            title={'Lorem Ipsum Dolor Sit Amet, Consetetur'}
+            children={'sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.'}
+            date={'30/10/2024'}
+            />
+
+            <Card
+            title={'Lorem Ipsum Dolor Sit Amet, Consetetur'}
+            children={'sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.'}
+            date={'30/10/2024'}
+            />
+
+            <Card
+            title={'Lorem Ipsum Dolor Sit Amet, Consetetur'}
+            children={'sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.'}
+            date={'30/10/2024'}
+            />
+          </div>
+
+        </Section>
       </Page>
     );
 };
